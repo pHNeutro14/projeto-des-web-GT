7c12ddfa237cddd71959a88ff192580cba38a45c
Pedro Henrique <111392329+pHNeutro14@users.noreply.github.com>
2024-12-23 15:33:21 +0000

    Criação das rotas

--- app.py
+++ app.py
@@ -4,4 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/adicionar_filme')
+def adicionar_filme():
+    return render_template('adicionar_filme.html')
+
+@app.route('/excluir_filme')
+def excluir_filme():
+    return render_template('excluir_filme.html')
+
+@app.route('pesquisar_filme')
+def pesquisar_filme():
+    return render_template('pesquisar_filme.html')
