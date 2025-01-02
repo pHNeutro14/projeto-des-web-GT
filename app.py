9cf74b1ca63ae8a4f1828f1e549678f6cb745c81
Pedro Henrique <111392329+pHNeutro14@users.noreply.github.com>
2025-01-02 12:43:52 +0000

    Criação do CREATE do crud

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import os
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
+from models.model_db import *
 
 from utilidades import *
 
@@ -17,18 +18,29 @@ conexao = f"mysql+pymysql://{dbusuario}:{dbsenha}@{host}/{meubanco}"
 app.config["SQLALCHEMY_DATABASE_URI"] = conexao 
 db.init_app(app) 
 
-@app.route('/')
+@app.route('/', methods = ["get", "post"])
 def index():
     return render_template('index.html')
 
-@app.route('/adicionar_filme')
+@app.route('/adicionar_filme', methods = ["get", "post"])
 def adicionar_filme():
     return render_template('adicionar_filme.html')
+
+@app.route('/enviar_filme', methods = ["get", "post"])
+def enviar_filme():
+    titulo = request.form.get('titulo')
+    genero = request.form.get('genero')
+    ano_lancamento = request.form.get('ano_lancamento')
+    avaliacao = request.form.get('avaliacao')
+    novo_filme = Filme(titulo = titulo, genero = genero, ano_lancamento = ano_lancamento, avaliacao = avaliacao)
+    db.session.add(novo_filme)
+    db.session.commit()
+    return render_template('enviar_filme.html', titulo = titulo, genero = genero, ano_lancamento = ano_lancamento, avaliacao = avaliacao)
 
 @app.route('/excluir_filme')
 def excluir_filme():
     return render_template('excluir_filme.html')
 
-@app.route('pesquisar_filme')
+@app.route('/pesquisar_filme')
 def pesquisar_filme():
     return render_template('pesquisar_filme.html')
